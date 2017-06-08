10ee1170e07ba4875f6297152d6914a27c28b01a
Jason Yeomans <jyeo098@gmail.com>
2017-06-07 22:24:49 -0400

    return loaner with creation of loan-offer

--- model/loan-offer/loan-offer-facade.js
+++ model/loan-offer/loan-offer-facade.js
@@ -1,30 +1,34 @@
 const Model = require('../../lib/facade');
-const loanOfferSchema  = require('./loan-offer-schema');
+const loanOfferSchema = require('./loan-offer-schema');
+const LoanOffer = require('./loan-offer-schema');
 
 
 class LoanOfferModel extends Model {
-	create(input) {
-    	const schema = new this.Schema(input);
-    	return schema.save((err) => {
-    		return this.Schema
-    	.findById(schema.id)
-    	.populate('loaner')
-    	.exec();
-    	});
-  	}
-  	findById(id) {
-    	return this.Schema
-    	.findById(id)
-    	.populate('loaner')
-    	.exec();
-  	}
-	find(query) {
-		console.log("Hi");
-    	return this.Schema
-    	.find(query)
-    	.populate('loaner')
-    	.exec();
-  	}
+  create(input) {
+    return new Promise((resolve, reject) => {
+      const loanOffer = new LoanOffer(input);
+      loanOffer.save((err) => {
+        LoanOffer.populate(loanOffer, {
+          path: "loaner"
+        }, function(err, loanOffer2) {
+          console.log(loanOffer2);
+          resolve(loanOffer2);
+        });
+      });
+    } );
+  }
+  findById(id) {
+    return this.Schema
+      .findById(id)
+      .populate('loaner')
+      .exec();
+  }
+  find(query) {
+    return this.Schema
+      .find(query)
+      .populate('loaner')
+      .exec();
+  }
 }
 
 module.exports = new LoanOfferModel(loanOfferSchema);
